bc1d28a8920dff54752bb6134ced6d8fb29e2bfc
Alexandre Studlé <alex.studle@gmail.com>
2017-05-11 19:04:21 +0200

    Cached ignore files removed

--- cache/compiled/files/01831f211057d710ab2ed1f696d09f89.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/PHP/grav-playground/user/config/security.yaml',
-    'modified' => 1494518197,
-    'data' => [
-        'salt' => 'GQlqNACnVLiRBr'
-    ]
-];
